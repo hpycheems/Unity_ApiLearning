31de840e5d5f03adb11ea5497c83ace9743c2b7a
luokangxing <1263645273@qq.com>
2023-09-01 15:16:48 +0800

    Application类

---
+++ Assets/Scripts/ApplicationAPI/ApplicationExample.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Application.dataPath);//E:/work/Unity_ApiLearning/Assets
+        Debug.Log(Application.isEditor);
+        Debug.Log(Application.isPlaying);
+        //C:/Users/Administrator/AppData/LocalLow/DefaultCompany/UnityAPILeaning
+        Debug.Log(Application.persistentDataPath);
+        //E:/ work / Unity_ApiLearning / Assets / StreamingAssets
+        Debug.Log(Application.streamingAssetsPath);
+        Debug.Log(Application.temporaryCachePath);
+
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quie();
+#endif
+        }
+    }
+}
